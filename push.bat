d39faa3de554b4ed0de00e540c5479d6f38d3c63
Yannick <yannick@kees-daun.de>
2021-11-26 01:18:37 +0100

    'change'

--- push.bat
+++ push.bat
@@ -1,5 +1,5 @@
 git init
 git add .
 git commit -m 'change'
-git remode add Masterarbeit 'https://github.com/Yannick-Kees/Masterarbeit'
+git remote add Masterarbeit 'http://github.com/Yannick-Kees/Masterarbeit'
 git push -u Masterarbeit master
