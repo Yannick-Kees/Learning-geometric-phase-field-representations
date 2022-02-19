6cb474801434c2981f4029a47a71aa840403a80d
Yannick-Kees <yannick@kees-daun.de>
2022-02-19 21:07:08 +0100

    Smaller

--- volta.py
+++ volta.py
@@ -20,7 +20,7 @@ EPSILON = .0001
 if LOSS == "MM":
     CONSTANT = 70.0 if not FOURIER_FEATUERS else 140.0 # 14, Modica Mortola
 else:
-    CONSTANT = 10. if FOURIER_FEATUERS else 5.0 # 14, Constante höher bei FF
+    CONSTANT = 10. if FOURIER_FEATUERS else 10.0 # 14, Constante höher bei FF
 MU = 0.0
 
 
