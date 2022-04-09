1157b9e2d7836ddf37c5f8acbc5f1bf166631592
Yannick-Kees <yannick@kees-daun.de>
2022-04-09 19:22:06 +0200

    2 layers

--- volta.py
+++ volta.py
@@ -29,52 +29,52 @@ MU = 0.5
 # Main #############
 ####################
 
-for l in range(1,8):
-
-    network = ParkEtAl(3, [NUM_NODES]*l, [4], FourierFeatures=FOURIER_FEATUERS, num_features = 8, sigma = SIGMA )
-    network.to(device) 
-    optimizer = optim.Adam(network.parameters(), START_LEARNING_RATE )
-    scheduler = ReduceLROnPlateau(optimizer, 'min', patience=PATIENCE, verbose=False)
-
-    file = open("3dObjects/bunny_0.ply")
-    pc = read_ply_file(file)
-    cloud = torch.tensor(normalize(pc))
-    #cloud = torch.tensor( flat_circle(2000) )
-
-    cloud += torch.tensor([0.15,-.15,.1]).repeat(cloud.shape[0],1)
-    cloud = torch.tensor(normalize(cloud) )
 
 
-    pc = Variable( cloud , requires_grad=True).to(device)
-    use_batch = (len(pc) > BATCHSIZE )
+network = ParkEtAl(3, [NUM_NODES]*2, [4], FourierFeatures=FOURIER_FEATUERS, num_features = 8, sigma = SIGMA )
+network.to(device) 
+optimizer = optim.Adam(network.parameters(), START_LEARNING_RATE )
+scheduler = ReduceLROnPlateau(optimizer, 'min', patience=PATIENCE, verbose=False)
 
-    for i in range(NUM_TRAINING_SESSIONS+1):
-        # training the network
-        # feed forward
-        # Omega = [0,1]^2
-        if use_batch:
-            
-            indices = np.random.choice(len(pc), BATCHSIZE, False)
-            pointcloud = pc[indices]
-        else:
-            pointcloud = pc
+file = open("3dObjects/bunny_0.ply")
+pc = read_ply_file(file)
+cloud = torch.tensor(normalize(pc))
+#cloud = torch.tensor( flat_circle(2000) )
+
+cloud += torch.tensor([0.15,-.15,.1]).repeat(cloud.shape[0],1)
+cloud = torch.tensor(normalize(cloud) )
+
+
+pc = Variable( cloud , requires_grad=True).to(device)
+use_batch = (len(pc) > BATCHSIZE )
+
+for i in range(NUM_TRAINING_SESSIONS+1):
+    # training the network
+    # feed forward
+    # Omega = [0,1]^2
+    if use_batch:
         
-        if LOSS == "AT":
-            loss = AT_loss(network, pointcloud, EPSILON, MONTE_CARLO_SAMPLES, MONTE_CARLO_BALL_SAMPLES, CONSTANT )
-            if (i%20==0):
-                report_progress(i, NUM_TRAINING_SESSIONS , loss.detach().cpu().numpy() )
-        else:
-            loss = Phase_loss(network, pointcloud, EPSILON, MONTE_CARLO_SAMPLES, MONTE_CARLO_BALL_SAMPLES, CONSTANT, MU)
-            if (i%10==0):
-                report_progress(i, NUM_TRAINING_SESSIONS , loss.detach().cpu().numpy() )
-        # report_progress(i, NUM_TRAINING_SESSIONS , loss.detach().cpu().numpy() )
-        
-        # backpropagation
-        network.zero_grad()
-        loss.backward()
-        optimizer.step()
-        scheduler.step(loss)
-        
-    #torch.save(network.state_dict(), "at40.pth")
-    toParaview(network, 256, l)
-    print("Finished"+str(l))
+        indices = np.random.choice(len(pc), BATCHSIZE, False)
+        pointcloud = pc[indices]
+    else:
+        pointcloud = pc
+    
+    if LOSS == "AT":
+        loss = AT_loss(network, pointcloud, EPSILON, MONTE_CARLO_SAMPLES, MONTE_CARLO_BALL_SAMPLES, CONSTANT )
+        if (i%20==0):
+            report_progress(i, NUM_TRAINING_SESSIONS , loss.detach().cpu().numpy() )
+    else:
+        loss = Phase_loss(network, pointcloud, EPSILON, MONTE_CARLO_SAMPLES, MONTE_CARLO_BALL_SAMPLES, CONSTANT, MU)
+        if (i%10==0):
+            report_progress(i, NUM_TRAINING_SESSIONS , loss.detach().cpu().numpy() )
+    # report_progress(i, NUM_TRAINING_SESSIONS , loss.detach().cpu().numpy() )
+    
+    # backpropagation
+    network.zero_grad()
+    loss.backward()
+    optimizer.step()
+    scheduler.step(loss)
+    
+#torch.save(network.state_dict(), "at40.pth")
+toParaview(network, 256, 2)
+print("Finished"+str(2))
